ca7da065b316f1d0efc8015b68c3ccf8d3c203b3
issemgane <ali.issemgane@gmail.com>
2018-10-21 15:07:39 +0000

    first commit upload project

---
+++ src/main/java/com/appstude/JwtSpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.appstude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringSecurityApplication.class, args);
+	}
+}
